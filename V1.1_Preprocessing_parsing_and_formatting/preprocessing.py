fca74ac725f129ce596c62caf68fbe6effc5e39f
Merlijn van Engelen <merlijnvanengelen@gmail.com>
2022-09-06 16:21:20 +0200

    Init: reading raw .edf data with MNE

---
+++ V1.1_Preprocessing_parsing_and_formatting/preprocessing.py
@@ -0,0 +1,14 @@
+import numpy as np
+import mne
+
+
+def info_raw_data():
+    print(raw_data)
+    print(raw_data.info)
+
+def plot_raw_data():
+    raw_data.plot(block=True)
+
+raw_data = mne.io.read_raw_edf('dataset/S001/S001R01.edf')
+info_raw_data()
+plot_raw_data()

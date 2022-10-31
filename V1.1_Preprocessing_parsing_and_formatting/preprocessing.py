0b988f3af35a807a93916935f71654ca54bf91cf
Merlijn van Engelen <merlijnvanengelen@gmail.com>
2022-10-31 16:12:15 +0100

    Use system argument

--- V1.1_Preprocessing_parsing_and_formatting/preprocessing.py
+++ V1.1_Preprocessing_parsing_and_formatting/preprocessing.py
@@ -1,14 +1,31 @@
 import numpy as np
 import mne
+import sys
 
+
+#T0 corresponds to rest
+#
+#T1 corresponds to onset of motion (real or imagined) of:
+#   - the left fist (in runs 3, 4, 7, 8, 11, and 12)
+#   - both fists (in runs 5, 6, 9, 10, 13, and 14)
+#
+#T2 corresponds to onset of motion (real or imagined) of
+#   - the right fist (in runs 3, 4, 7, 8, 11, and 12)
+#   - both feet (in runs 5, 6, 9, 10, 13, and 14)
+
+baseline = [1, 2]
+left_or_hands = [3, 4, 7, 8, 11, 12]
+right_or_feet = [5, 6, 9, 10, 13, 14]
 
 def info_raw_data():
     print(raw_data)
     print(raw_data.info)
 
 def plot_raw_data():
-    raw_data.plot(block=True)
+    raw_data.plot(duration=60, use_opengl=True,block=True)
 
-raw_data = mne.io.read_raw_edf('S001R01.edf')
+print(sys.argv)
+raw_data = mne.io.read_raw_edf(sys.argv[1])
+events = mne.events_from_annotations(raw_data)
 info_raw_data()
 plot_raw_data()

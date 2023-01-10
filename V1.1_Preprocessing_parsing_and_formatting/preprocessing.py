0244835ab688ce7c7dbd24d9f9387a55454b324a
Merlijn van Engelen <merlijnvanengelen@gmail.com>
2023-01-10 18:08:51 +0100

    Documentation implementation of CSP & LDA with sklearn Pipeline

--- V1.1_Preprocessing_parsing_and_formatting/preprocessing.py
+++ V1.1_Preprocessing_parsing_and_formatting/preprocessing.py
@@ -1,8 +1,6 @@
 import mne
 import sys
-import matplotlib.pyplot as plt
 from autoreject import get_rejection_threshold
-from pathlib import Path
 import argparse
 
 # T0 corresponds to rest
@@ -26,26 +24,28 @@ hands_or_feet = [5, 6, 9, 10, 13, 14]
 
 def parse_args():
     parser = argparse.ArgumentParser(
-                        prog = 'Epoching',
-                        description = 'Segment data into epochs')
-    parser.add_argument('-s', '--subject', type=int, required=True) 
-    parser.add_argument('-r', '--run', type=int, required=True) 
+        prog='Epoching',
+        description='Segment data into epochs')
+    parser.add_argument('-s', '--subject', type=int, required=True)
+    parser.add_argument('-r', '--run', type=int, required=True)
     args = parser.parse_args()
     if args.subject > 109 or args.subject < 1:
         sys.exit("Invalid subject number")
     if args.run in left_or_right:
-        event_mapping = {'Rest':1, 'Left fist':2, 'Right fist':3}
+        event_mapping = {'Rest': 1, 'Left fist': 2, 'Right fist': 3}
     elif args.run in hands_or_feet:
-        event_mapping = {'Rest':1, 'Both hands':2, 'Both feet':3}
+        event_mapping = {'Rest': 1, 'Both hands': 2, 'Both feet': 3}
     else:
         sys.exit("Invalid run number")
     return args, event_mapping
+
 
 def format_filename(args):
     subject = "S{:03d}".format(args.subject)
     run = "R{:02d}".format(args.run)
     filename = subject + "/" + subject + run + ".edf"
     return filename
+
 
 args, event_mapping = parse_args()
 filename = format_filename(args)
@@ -63,72 +63,118 @@ raw.set_montage("standard_1005")
 # Read events from raw
 events, event_dict = mne.events_from_annotations(raw)
 
-######## FILTERING
+# FILTERING
 
-# Standard filter
-low_cut = 0.1
-hi_cut = 30
-raw_filt = raw.copy().filter(low_cut, hi_cut)
 
-# ICA (Independent Component Analysis) filter
-ica_low_cut = 1
-hi_cut = 30
-raw_ica = raw.copy().filter(ica_low_cut, hi_cut)
+def standard_filter(raw):
+    low_cut = 0.1
+    hi_cut = 15
+    raw_filt = raw.copy().filter(low_cut, hi_cut)
+    raw_filt.save(filepath + '-filt-raw.fif', overwrite=True)
+    return raw_filt
 
-######## EPOCHING
+
+def ica_filter(raw):
+    ica_low_cut = 1
+    hi_cut = 15
+    raw_ica = raw.copy().filter(ica_low_cut, hi_cut)
+    return raw_ica
+
+
+raw_filt = standard_filter(raw)
+raw_ica = ica_filter(raw)
+
+# EPOCHING FOR ICA
 
 # Break raw data into 1 s epochs
-# tstep = 1.0
-# events_ica = mne.make_fixed_length_events(raw_ica, duration=tstep)
-# epochs_ica = mne.Epochs(raw_ica, events_ica, tmin=0.0,
-#                         tmax=tstep, baseline=None, preload=True)
+tstep = 1.0
+events_ica = mne.make_fixed_length_events(raw_ica, duration=tstep)
+epochs_ica = mne.Epochs(raw_ica, events_ica, tmin=0.0,
+                        tmax=tstep, baseline=None, preload=True)
 
 
 # ######## ICA
 
-# # Get rejection threshold (excessively noisy)
-# reject = get_rejection_threshold(epochs_ica)
-# print(reject)
+def ica():
+    # # Get rejection threshold (excessively noisy)
+    reject = get_rejection_threshold(epochs_ica)
+    print(reject)
 
-# # ICA parameters
-# random_state = 42   # ensures ICA is reproducable each time it's run
-# # Specify n_components as a decimal to set % explained variance
-# ica_n_components = .99
+    # ICA parameters
+    random_state = 42   # ensures ICA is reproducable each time it's run
+    # Specify n_components as a decimal to set % explained variance
+    ica_n_components = .99
 
-# # Fit ICA
-# ica = mne.preprocessing.ICA(
-#     n_components=ica_n_components, random_state=random_state)
-# ica.fit(epochs_ica,
-#         reject=reject,
-#         tstep=tstep)
+    # Fit ICA
+    ica = mne.preprocessing.ICA(
+        n_components=ica_n_components, random_state=random_state)
+    ica.fit(epochs_ica,
+            reject=reject,
+            tstep=tstep)
 
-# # Plot ICA components (set picks argument to show more than 5)
-# ica.plot_properties(epochs_ica, psd_args={'fmax': hi_cut})
+    # Plot ICA components (set picks argument to show more than 5)
+    # ica.plot_properties(epochs_ica, psd_args={'fmax': hi_cut})
 
-# # Find and exclude Electro Oculogram (EOG) components (blinks, eye movements)
-# ica_z_thresh = 1.96
-# eog_indices, eog_scores = ica.find_bads_eog(raw_ica,
-#                                             ch_name=['Fp1', 'F8'],
-#                                             threshold=ica_z_thresh)
-# ica.exclude = eog_indices
+    # Find and exclude Electro Oculogram (EOG) components (blinks, eye movements)
+    ica_z_thresh = 1.96
+    eog_indices, eog_scores = ica.find_bads_eog(raw_ica,
+                                                ch_name=['Fp1', 'F8'],
+                                                threshold=ica_z_thresh)
+    ica.exclude = eog_indices
 
-# # Save ICA
-# # ica.save(filepath + '-ica.fif',
-# #          overwrite=True)
+    # Save ICA
+    ica.save(filepath + '-ica.fif',
+             overwrite=True)
+    return ica
 
-######## PLOTTING
+# PLOTTING
 
-# Plot EOG scores (red = bad)
+# # Plot EOG scores (red = bad)
 # ica.plot_scores(eog_scores)
 
-# Create power spectral density plot
+# # Create power spectral density plot
 # raw_filt.plot_psd()
 
-# Create time domain plot
+# # Create time domain plot
 # raw_filt.plot(block=True)
 
 
-######## SAVE
+# SAVE
 # Save .fif files
-raw_filt.save(filepath + '-filt-raw.fif', overwrite=True)
 # raw.save(filepath + '-raw.fif', overwrite=True)
+
+def epoching(ica):
+    # Epoching settings
+    tmin = -.200  # start of each epoch (in sec)
+    tmax = 1.000  # end of each epoch (in sec)
+    baseline = (None, 0)
+
+    # Create epochs
+    epochs = mne.Epochs(raw_filt,
+                        events, event_mapping,
+                        tmin, tmax,
+                        baseline=baseline,
+                        preload=True
+                        )
+    print(epochs)
+
+    # Butterfly plot (average over all epochs)
+    epochs.average().plot(spatial_colors=True)
+
+    # ica = mne.preprocessing.read_ica(filepath + '-ica.fif')
+
+    epochs_post_ica = ica.apply(epochs.copy())
+    epochs_post_ica.save(filepath + '-epo.fif', overwrite=True)
+    return epochs_post_ica
+
+
+def evokeds(epochs_post_ica):
+    evokeds = {c: epochs_post_ica[c].average() for c in event_mapping}
+    mne.write_evokeds(filepath + '-ave.fif', list(evokeds.values()), overwrite=True)
+
+    return evokeds
+
+
+ica_loaded = ica()
+epochs_post_ica = epoching(ica_loaded)
+evokeds = evokeds(epochs_post_ica)
